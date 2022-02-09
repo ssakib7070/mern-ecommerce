fd04b6a805defbe2ab32d3c79c4c759b86fe98e3
Sadman Sakib <ssakib7070@gmail.com>
2022-02-09 01:47:30 -0600

    4. MongoDB

--- backend/server.js
+++ backend/server.js
@@ -1,22 +1,23 @@
 import express from "express";
-import products from "./data/products.js";
 import dotenv from "dotenv";
+import connectDB from "./config/db.js";
+import productRoutes from "./routes/productRoutes.js";
+import { errorHandler, notFound } from "./middleware/errorMiddleware.js";
 
 const PORT = process.env.PORT || 5000;
 const app = express();
+
 dotenv.config();
+connectDB();
 
 app.get("/", (req, res) => {
   res.send("API is running.....");
 });
 
-app.get("/api/products", (req, res) => {
-  res.json(products);
-});
+// app.use(notFound);
 
-app.get("/api/products/:id", (req, res) => {
-  const product = products.find((p) => p._id === req.params.id);
-  res.json(product);
-});
+// app.use(errorHandler);
+
+app.use("/api/products", productRoutes);
 
 app.listen(PORT, console.log(`Server running on PORT ${PORT}`));
